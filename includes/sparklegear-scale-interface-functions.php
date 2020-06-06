07ff3c505fd51e3cdd4ac6d10bd036cf1df05190
Semifake <42242696+Semifake@users.noreply.github.com>
2020-06-06 14:12:22 -0400

    Import code

---
+++ includes/sparklegear-scale-interface-functions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Add a menu link and page to the admin control panel
+*/
+
+add_action( 'admin_menu', sparklegear_scale_interface_add_acp_page );
+
+/**
+ * Add menu link and page to the admin control panel
+ * 
+ * @since   1.0.0
+ */
+
+function sparklegear_scale_interface_add_acp_page() {
+    add_menu_page(
+        'Sparklegear Scale Interface',
+        'Scale Interface',
+        'manage_options',
+        'includes/sparklegear-scale-interface-acp-page.php'
+    );
+}
