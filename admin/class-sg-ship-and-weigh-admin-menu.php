7ce91c791fb292c092f9de9de3de67c45d587f4f
Semifake <42242696+Semifake@users.noreply.github.com>
2020-06-14 13:24:21 -0400

    Refactor menu functions

--- admin/class-sg-ship-and-weigh-admin-menu.php
+++ admin/class-sg-ship-and-weigh-admin-menu.php
@@ -9,15 +9,6 @@ defined( 'ABSPATH' ) or die( 'Direct access blocked.' );
 class SG_Ship_And_Weigh_Admin_Menu {
 
     /**
-     * Settings menu slug
-     * 
-     * @since 1.0.0
-     * 
-     * @var string
-     */
-    protected string $settings_slug = 'sg-ship-and-weigh-settings-menu';
-
-    /**
      * Shipping menu slug
      * 
      * @since 1.0.0
@@ -25,6 +16,15 @@ class SG_Ship_And_Weigh_Admin_Menu {
      * @var string
      */
     protected string $shipping_slug = 'sg-ship-and-weigh-shipping-menu';
+
+    /**
+     * Settings menu slug
+     * 
+     * @since 1.0.0
+     * 
+     * @var string
+     */
+    protected string $settings_slug = 'sg-ship-and-weigh-settings-menu';
 
     /**
      * URL of the admin includes and assets
@@ -65,48 +65,8 @@ class SG_Ship_And_Weigh_Admin_Menu {
      * @since 1.0.0
      */
     public function init_hooks() {
-        add_action( 'admin_menu', array( $this, 'add_settings_menu' ) );
         add_action( 'admin_menu', array( $this, 'add_shipping_menu' ) );
-    }
-
-    /**
-     * Create a menu page with all associated scripts and styles
-     * 
-     * @since 1.0.0
-     * 
-     * @param string $slug Slug of the page to be created
-     * // TODO: Make a new datatype or add a function to document attrs
-     * @param array $menu Settings passed to add_menu_page()
-     * @param string $page_url URL from assets_root_url to the page php file
-     * @param string $script_url URL from assets_root_url to the page script
-     * @param string $style_url URL from assets_root_url to the page stylesheet
-     * @param string $object_name JS object name to be passed to wp_localize_script()
-     * @param string $object JS object to be passed to wp_localize_script()
-     */
-    public function create_new_admin_page(
-        string $slug,
-        array  $menu,
-        string $page_url,
-        string $script_url,
-        string $style_url,
-        string $object_name,
-        array  $object
-    ) {
-        $page = add_menu_page(
-            $menu['page_title'],
-            $menu['menu_title'],
-            $menu['capability'],
-            $slug,
-            function() use ( $slug, $page_url, $script_url, 
-                             $style_url, $object_name, $object ) {
-                $this->enqueue_assets(
-                    $slug,
-                    $script_url, $style_url,
-                    $object_name, $object
-                );
-                include( $this->assets_root_path . $page_url );
-            }
-        );
+        add_action( 'admin_menu', array( $this, 'add_settings_menu' ) );
     }
 
     /**
@@ -131,19 +91,49 @@ class SG_Ship_And_Weigh_Admin_Menu {
     }
 
     /**
-     * Create plugin settings menu
+     * Load and enqueue assets for shipping_menu
      * 
      * @since 1.0.0
      */
-    public function add_settings_menu() {
-        $this->create_new_admin_page(
-            'sg-ship-and-weigh-settings',
+    public function load_shipping_menu() {
+        $this->enqueue_assets(
+            $this->shipping_slug,
+            'js/sg-ship-and-weigh-shipping-menu.js',
+            'css/sg-ship-and-weigh-shipping-menu.css',
+            'SHIP_AND_WEIGH',
             array(
-                'page_title' => 'Settings - Ship and Weigh',
-                'menu_title' => 'Ship and Weigh Settings',
-                'capability' => 'manage_options'
+                'strings' => array(
+                    'saved' => __( 'Settings Saved', 'text-domain' ),
+                    'error' => __( 'Error', 'text-domain' ),
+                ),
             ),
-            'pages/sg-ship-and-weigh-settings-menu.php',
+        );
+        include( $this->assets_root_path . 'pages/sg-ship-and-weigh-shipping-menu.php' );
+    }
+
+    /**
+     * Create plugin shipping menu
+     * 
+     * @since 1.0.0
+     */
+    public function add_shipping_menu() {
+        add_menu_page(
+            'Shipping - Ship and Weigh',
+            'Ship and Weigh',
+            'manage_options',
+            $this->shipping_slug,
+            array( $this, 'load_shipping_menu' ),
+        );
+    }
+
+    /**
+     * Load and enqueue assets for settings_menu
+     * 
+     * @since 1.0.0
+     */
+    public function load_settings_menu() {
+        $this->enqueue_assets(
+            $this->settings_slug,
             'js/sg-ship-and-weigh-settings-menu.js',
             'css/sg-ship-and-weigh-settings-menu.css',
             'SHIP_AND_WEIGH',
@@ -156,37 +146,26 @@ class SG_Ship_And_Weigh_Admin_Menu {
                     'url'   => esc_url_raw( rest_url( 'sg-ship-and-weigh-api/v1/settings' ) ),
                     'nonce' => wp_create_nonce( 'wp_rest' ),
                 ),
-            )
+            ),
         );
+        include( $this->assets_root_path . 'pages/sg-ship-and-weigh-settings-menu.php' );
     }
 
     /**
-     * Create plugin shipping menu
+     * Create plugin settings menu
      * 
      * @since 1.0.0
      */
-    public function add_shipping_menu() {
-        $this->create_new_admin_page(
-            'sg-ship-and-weigh-shipping',
-            array(
-                'page_title' => 'Shipping - Ship and Weigh',
-                'menu_title' => 'Ship and Weigh',
-                'capability' => 'manage_options'
-            ),
-            'pages/sg-ship-and-weigh-shipping-menu.php',
-            'js/script.js',
-            'css/style.css',
-            'SHIP_AND_WEIGH',
-            array(
-                'strings' => array(
-                    'saved' => __( 'Settings Saved', 'text-domain' ),
-                    'error' => __( 'Error', 'text-domain' ),
-                ),
-                'api'     => array(
-                    'url'   => esc_url_raw( rest_url( 'sg-ship-and-weigh-api/v1/settings' ) ),
-                    'nonce' => wp_create_nonce( 'wp_rest' ),
-                ),
-            )
+    public function add_settings_menu() {
+        add_submenu_page(
+            $this->shipping_slug,
+            'Settings - Ship and Weigh',
+            'Settings',
+            'manage_options',
+            $this->settings_slug,
+            array( $this, 'load_settings_menu' ),
         );
     }
+
+
 }
