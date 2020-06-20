35f1f68466968666f0574ff518d42ed911af8a1b
Semifake <42242696+Semifake@users.noreply.github.com>
2020-06-20 16:56:40 -0400

    Add Select2 to a dropdown

--- admin/js/sg-ship-and-weigh-shipping-menu.js
+++ admin/js/sg-ship-and-weigh-shipping-menu.js
@@ -1,6 +1,24 @@
-let app = new Vue( {
+let app = new Vue({
     el: '#root',
     data: {
         weight_type: 'pounds_and_ounces' // Or 'decimal_pounds'
     },
-} );
+});
+
+jQuery( $ => {
+    $( '#recipient-name' ).select2({
+        tags: true,
+        data: {
+             "results": [
+                {
+                    "id": 1,
+                    "text": 'option1',
+                },
+                {
+                    "id": 2,
+                    "text": 'option2'
+                },
+             ]
+        },
+    });
+});
