b3181cbc7548ac9991f20edbd22d00aee2b55996
Semifake <42242696+Semifake@users.noreply.github.com>
2020-06-20 09:23:51 -0400

    Use VueJS and jQuery to get and display settings

--- admin/js/sg-ship-and-weigh-settings-menu.js
+++ admin/js/sg-ship-and-weigh-settings-menu.js
@@ -1,1 +1,22 @@
-console.log("sg-ship-and-weigh TEST");
+let app = new Vue( {
+    el: '#root',
+    data: {
+        settings: SHIP_AND_WEIGH.settings_spec
+    }
+} );
+
+jQuery( $ => {
+    $.ajax( {
+        method: 'GET',
+        url: SHIP_AND_WEIGH.api.url,
+        beforeSend: ( xhr ) => {
+            xhr.setRequestHeader( 'X-WP-Nonce', SHIP_AND_WEIGH.api.nonce );
+        },
+    } ).then( ( response ) => {
+        for ( let [setting, setting_data] of Object.entries(app.$data.settings) ) {
+            if ( response.hasOwnProperty( setting ) ) {
+                setting_data.value = response.setting;
+            }
+        }
+    } )
+} );
