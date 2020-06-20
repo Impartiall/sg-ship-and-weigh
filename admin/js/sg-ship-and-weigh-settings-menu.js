f60ddacdced4a2e70fcd49a0464ee7a42a4b5344
Semifake <42242696+Semifake@users.noreply.github.com>
2020-06-20 16:21:16 -0400

    Fix POST request

--- admin/js/sg-ship-and-weigh-settings-menu.js
+++ admin/js/sg-ship-and-weigh-settings-menu.js
@@ -1,22 +1,50 @@
-let app = new Vue( {
+let app = new Vue({
     el: '#root',
     data: {
-        settings: SHIP_AND_WEIGH.settings_spec
+        settings: SHIP_AND_WEIGH.settings_spec,
+        feedback: '',
     }
-} );
+});
 
 jQuery( $ => {
-    $.ajax( {
+    $.ajax({
         method: 'GET',
         url: SHIP_AND_WEIGH.api.url,
         beforeSend: ( xhr ) => {
             xhr.setRequestHeader( 'X-WP-Nonce', SHIP_AND_WEIGH.api.nonce );
         },
-    } ).then( ( response ) => {
-        for ( let [setting, setting_data] of Object.entries(app.$data.settings) ) {
-            if ( response.hasOwnProperty( setting ) ) {
-                setting_data.value = response.setting;
+    }).then( response => {
+            for ( let [ setting, setting_data ] of Object.entries( app.$data.settings ) ) {
+                if ( response.hasOwnProperty( setting ) ) {
+                    setting_data.value = response.setting;
+                }
             }
+    });
+
+    $( '#settings-form' ).on( 'submit', e => {
+        e.preventDefault();
+
+        let data = {};
+        for ( let [ setting, setting_data ] of Object.entries( app.$data.settings ) ) {
+            data[setting] = setting_data.value;
         }
-    } )
-} );
+        console.log(data);
+
+        $.ajax({
+            method: 'POST',
+            url: SHIP_AND_WEIGH.api.url,
+            beforeSend: ( xhr ) => {
+                xhr.setRequestHeader( 'X-WP-Nonce', SHIP_AND_WEIGH.api.nonce );
+            },
+            data: data,
+            error: response => {
+                app.$data.feedback = SHIP_AND_WEIGH.strings.error;
+                if ( response.hasOwnProperty( 'message' ) ) {
+                    app.$data.feedback = response.message;
+                }
+            },
+        }).then( response => {
+                app.$data.feedback = SHIP_AND_WEIGH.strings.saved;
+        });
+    });
+});
