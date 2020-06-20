308125db7e6d7c58cfed12240dcb42abaf8461fc
Semifake <42242696+Semifake@users.noreply.github.com>
2020-06-20 19:59:29 -0400

    Add methods to get and add recipients

---
+++ admin/class-sg-ship-and-weigh-shipping-settings.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Handle adding, retriving, and deleting of remembered shipping info
+ * 
+ * @since 1.0.0
+ */
+defined( 'ABSPATH' ) or die( 'Direct access blocked.' );
+
+class SG_Ship_And_Weigh_Shipping_Settings {
+
+    /**
+     * Generic option key to be suffixed for each setting
+     * 
+     * @since 1.0.0
+     * 
+     * @var string
+     */
+    protected string $option_key = '_sg_ship_and_weigh_';
+
+    /**
+     * Required attributes for recipients
+     * 
+     * @since 1.0.0
+     * 
+     * @var array
+     */
+    protected array $recipient_required_keys = [
+        'name',
+        'email',
+        'country',
+        'address',
+    ];
+
+    /**
+     * Get an array of recipients and their addresses from the database
+     * 
+     * @since 1.0.0
+     * 
+     * @return array
+     */
+    public function get_recipients() {
+        $recipients = get_option( $option_key . 'recipients', array() );
+        return $recipients;
+    }
+
+    /**
+     * Add a recipient and associated data to the database
+     * 
+     * @since 1.0.0
+     * 
+     * @param array $recipient Recipient data to be added to the database
+     */
+    public function add_recipient( array $recipient ) {
+        $recipients = $this->get_recipients();
+
+        if ( ! ( array_keys( $recipient ) === $this->recipient_required_keys ) ) {
+            return 'Invalid recipient';
+        }
+
+        $recipients[] = $recipient;
+        update_option( $this->option_key . 'recipients', $recipients );
+    }
+}
