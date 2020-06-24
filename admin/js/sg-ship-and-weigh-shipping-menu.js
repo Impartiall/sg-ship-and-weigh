0e8ee9e5995d1fb489dd338e94becb41b144745c
Semifake <42242696+Semifake@users.noreply.github.com>
2020-06-24 15:50:39 -0400

    Setup select box

--- admin/js/sg-ship-and-weigh-shipping-menu.js
+++ admin/js/sg-ship-and-weigh-shipping-menu.js
@@ -32,91 +32,48 @@ let app = new Vue({
 
 jQuery( $ => {
     $( '#recipient-name' ).selectize({
-        valueField: 'url',
+        create: true,
         labelField: 'name',
-        searchField: 'name',
-        create: false,
+        valueField: 'text',
+        searchField: [ 'name', 'text' ],
         render: {
-            option: function(item, escape) {
-                return '<div>' +
-                    '<span class="title">' +
-                        '<span class="name"><i class="icon ' + (item.fork ? 'fork' : 'source') + '"></i>' + escape(item.name) + '</span>' +
-                        '<span class="by">' + escape(item.username) + '</span>' +
-                    '</span>' +
-                    '<span class="description">' + escape(item.description) + '</span>' +
-                    '<ul class="meta">' +
-                        (item.language ? '<li class="language">' + escape(item.language) + '</li>' : '') +
-                        '<li class="watchers"><span>' + escape(item.watchers) + '</span> watchers</li>' +
-                        '<li class="forks"><span>' + escape(item.forks) + '</span> forks</li>' +
-                    '</ul>' +
-                '</div>';
-            }
+            item: ( item, escape ) => {
+                return `<div>
+                    <span class="name">${ escape( item.name ) }</span>
+                    <span class="text">(${ escape( item.text ) })</span>
+                </div>`
+            },
+            option: ( item, escape ) => {
+                return `<div>
+                    <span class="name">${ escape( item.name ) }</span>
+                    <span class="text">${ escape( item.text ) }</span>
+                </div>`
+            },
         },
-        score: function(search) {
-            var score = this.getScoreFunction(search);
-            return function(item) {
-                return score(item) * (1 + Math.min(item.watchers / 100, 1));
-            };
-        },
-        load: function(query, callback) {
-            if (!query.length) return callback();
+        load: ( query, callback ) => {
+            console.log( 'Loading options for #recipient-name' );
+            if ( !query.length ) return callback();
             $.ajax({
-                url: 'https://api.github.com/legacy/repos/search/' + encodeURIComponent(query),
+                url: SHIP_AND_WEIGH.api.recipients_url,
                 type: 'GET',
-                error: function() {
-                    callback();
+                beforeSend: xhr => {
+                    xhr.setRequestHeader( 'X-WP-Nonce', SHIP_AND_WEIGH.api.nonce );
                 },
-                success: function(res) {
-                    callback(res.repositories.slice(0, 10));
-                }
+                error: response => {
+                    callback();
+                    app.$data.feedback = SHIP_AND_WEIGH.strings.error;
+                    if ( response.hasOwnProperty( 'message' ) ) {
+                        app.$data.feedback = response.message;
+                    }
+                },
+                success: data => {
+                    console.log( data );
+                    callback( data );
+                    app.$data.feedback = SHIP_AND_WEIGH.strings.recipient_added;
+                },
             });
         }
     });
-    
-    // .selectize({
-    //     persistent: false,
-    //     labelField: 'name',
-    //     valueField: 'text',
-    //     searchField: [ 'name', 'text' ],
-    //     create: true,
-    //     render: {
-    //         item: ( item, escape ) => {
-    //             return `<div>
-    //                 <span class="name">${ escape( item.name ) }</span>
-    //                 <span class="text">(${ escape( item.text ) })</span>
-    //             </div>`
-    //         },
-    //         option: ( item, escape ) => {
-    //             return `<div>
-    //                 <span class="name">${ escape( item.name ) }</span>
-    //                 <span class="text">${ escape( item.text ) }</span>
-    //             </div>`
-    //         },
-    //     },
-    //     load: ( query, callback ) => {
-    //         console.log( 'Loading options for #recipient-name' );
-    //         if ( !query.length ) return callback();
-    //         $.ajax({
-    //             url: SHIP_AND_WEIGH.api.recipients_url,
-    //             type: 'GET',
-    //             beforeSend: xhr => {
-    //                 xhr.setRequestHeader( 'X-WP-Nonce', SHIP_AND_WEIGH.api.nonce );
-    //             },
-    //             error: response => {
-    //                 callback();
-    //                 app.$data.feedback = SHIP_AND_WEIGH.strings.error;
-    //                 if ( response.hasOwnProperty( 'message' ) ) {
-    //                     app.$data.feedback = response.message;
-    //                 }
-    //             },
-    //             success: data => {
-    //                 console.log( data );
-    //                 callback( data );
-    //                 app.$data.feedback = SHIP_AND_WEIGH.strings.recipient_added;
-    //             },
-    //         });
-    //     }
-    // });
 
     $( '#add-recipient' ).on( 'click', e => {
         e.preventDefault();
