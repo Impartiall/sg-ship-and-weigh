66d0e08c3a7ff876f91e72e59146004617acc1af
Semifake <42242696+Semifake@users.noreply.github.com>
2020-07-03 09:28:45 -0400

    Add sender address options

--- admin/pages/sg-ship-and-weigh-settings-menu.php
+++ admin/pages/sg-ship-and-weigh-settings-menu.php
@@ -1,17 +1,45 @@
 <div id="root" class="wrap">
+    <form>
+        <ul>
+            <li>
+                <label for="name">Name / Company</label>
+                <select name="name" id="sender-name"></select>
+
+                <button class="button button-secondary" id="add-sender">Add to database</button>
+            </li>
+            <li>
+                <label for="sender-street1">Address Line 1</label>
+                <input type="text" name="sender-street1" id="sender-street1" v-model="sender.address.street1">
+                <a id="sender-address-feedback">{{ sender.address_feedback }}</a>
+            </li>
+            <li>
+                <label for="sender-street2">Address Line 2</label>
+                <input type="text" name="sender-street2" id="sender-street2" v-model="sender.address.street2">
+            </li>
+            <li>
+                <label for="sender-city">City</label>
+                <input type="text" name="sender-city" id="sender-city" v-model="sender.address.city">
+            </li>
+            <li>
+                <label for="sender-state">State</label>
+                <input type="text" name="sender-state" id="sender-state" v-model="sender.address.state">
+            </li>
+            <li>
+                <label for="sender-zip">Postal Code</label>
+                <input type="text" name="sender-zip" id="sender-zip" v-model="sender.address.zip">
+            </li>
+            <li>
+                <label for="sender-country">Country</label>
+                <select name="sender-country" id="sender-country">
+                </select>
+            </li>
+        </ul>
+    </form>
     <form id="settings-form">
         <ul>
             <li v-for="(setting_data, setting) in settings">
                 <label :for="setting">{{ setting_data.name }}</label>
                 <input :type="setting_data.html_type" v-model="setting_data.value" :name="setting" :id="setting">
-            </li>
-            <li>
-                <label for="x"> Test</label>
-                <input type="text">
-            </li>
-            <li>
-                <label for="y">test area</label>
-                <textarea name="y" id="" cols="30" rows="10"></textarea>
             </li>
         </ul>
         <button type="submit" class="button button-primary">Submit</button>
