25ef8189842a9389d7e4bf3bc609ddbfbf589e45
Semifake <42242696+Semifake@users.noreply.github.com>
2020-07-03 11:55:20 -0400

    Add settings

--- admin/class-sg-ship-and-weigh-settings-specification.php
+++ admin/class-sg-ship-and-weigh-settings-specification.php
@@ -16,12 +16,45 @@ class SG_Ship_And_Weigh_Settings_Specification {
      * @var array
      */
     protected static array $settings_blueprint = array(
-        'setting' => [
-            'Setting',
-            0,
-            'int',
-            'number',
-            'absint',
+        'default_weight_mode' => [
+            '',
+            'string',
+            'sanatize_text_field',
+        ],
+        'name' => [
+            '',
+            'string',
+            'sanatize_text_field',
+        ],
+        'street1' => [
+            '',
+            'string',
+            'sanatize_text_field',
+        ],
+        'street2' => [
+            '',
+            'string',
+            'sanatize_text_field',
+        ],
+        'city' => [
+            '',
+            'string',
+            'sanatize_text_field',
+        ],
+        'state' => [
+            '',
+            'string',
+            'sanatize_text_field',
+        ],
+        'zip' => [
+            '',
+            'string',
+            'sanatize_text_field',
+        ],
+        'country' => [
+            '',
+            'string',
+            'sanatize_text_field',
         ],
     );
 
@@ -48,24 +81,19 @@ class SG_Ship_And_Weigh_Settings_Specification {
      * 
      * @since 1.0.0
      * 
-     * @param string $name Name of the setting to be displayed in HTML
      * @param mixed $default The default value for the setting
      * @param string $type The type of the setting
-     * @param string $html_type The type for the corresponding HTML input
      * @param string sanatize_callback
      *     The callback with which to sanatize input to this setting
      * 
      * @return array
      */
     public static function get_setting_specification(
-        string $name, $default, string $type,
-        string $html_type, string $sanatize_callback
+        $default, string $type, string $sanatize_callback
     ) {
         return array(
-            'name' => __( $name, 'text-domain' ),
             'default' => $default,
             'type' => $type,
-            'html_type' => $html_type,
             'sanatize_callback' => $sanatize_callback,
         );
     }
